97c744529bce6ea3993848a63c38e68cc6c5f71b
YasminVargas <yasmiin.vargass@gmail.com>
2023-09-29 18:03:42 -0300

    created hed and footer components, and global css

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <?php
+    require_once "./components/head.php";
+    ?>
+    <title>MovieStar</title>
+</head>
+
+<body>
+    <h1>test</h1>
+
+
+    <?php
+require_once "./components/footer.php";
+    ?>
+
+</body>
+
+</html>
