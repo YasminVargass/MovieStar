e0317eba8908381ddb637501fe184765b483fbd4
YasminVargas <yasmiin.vargass@gmail.com>
2023-10-01 19:10:33 -0300

    updated header w css

---
+++ components/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<body>
+
+    <header>
+        <nav class="navbar bg-dark">
+            <a href="../index.php">
+                <img class="logo" src="./assets/images/logo.svg" alt="Logo de estrela do site MovieStar">
+                MovieStar
+            </a>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+            </form>
+        </nav>
+    </header>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -5,10 +5,14 @@
     <?php
     require_once "./components/head.php";
     ?>
+    <link rel="stylesheet" href="./assets/css/header.css">
     <title>MovieStar</title>
 </head>
 
 <body>
+    <?php
+    require_once "./components/header.php";
+    ?>
     <h1>test</h1>
 
 
