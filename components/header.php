32c3a34fd48cfafa6f6c1e7708ff3b496630b39a
YasminVargas <yasmiin.vargass@gmail.com>
2023-10-06 18:58:35 -0300

    finished non-user header

--- components/header.php
+++ components/header.php
@@ -5,17 +5,26 @@
 
     <header>
         <nav class="navbar bg-dark">
-            <div class="">
-                <a href="../index.php">
+            <div>
+                <a href="../index.php" class="logo-container">
                     <img class="logo" src="./assets/images/logo.svg" alt="Logo de estrela do site MovieStar">
                     MovieStar
                 </a>
             </div>
-            <form class="container-fluid">
+            <form class="container-fluid ms-5">
                 <div class="input-group">
                     <input type="text" class="form-control" placeholder="Buscar Filmes" aria-label="Buscar Filmes" aria-describedby="basic-addon1">
-                    <span class="input-group-text" id="basic-addon1"><button><i class="fa fa-search" aria-hidden="true"></i></button></span>
+                    <span class="input-group-text" id="basic-addon1">
+                        <button class="search-btn">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                        </button>
+                    </span>
                 </div>
+            </form>
+            <div class="user-actions">
+                <a href="#" class="sign-in">Entrar</a>
+                <a href="#" class="sign-up">Cadastrar</a>
+            </div>
         </nav>
     </header>
 
