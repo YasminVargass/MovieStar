823e17d8284c5b069df02c31c40c9135b383955d
YasminVargas <yasmiin.vargass@gmail.com>
2023-10-09 21:18:26 -0300

    updated sign-up page

--- auth.php
+++ auth.php
@@ -33,6 +33,31 @@
             </div>
             <div class="col-md-4">
                 <h2>Registrar</h2>
+                <form action="#">
+                    <div>
+                        <label class="form-label" for="email">E-mail:</label>
+                        <input class="form-control" type="email" id="email" name="email" placeholder="E-mail">
+                    </div>
+                    <div>
+                        <label class="form-label" for="name">Nome:</label>
+                        <input class="form-control" type="text" id="name" name="name" placeholder="Digite seu nome">
+                    </div>
+                    <div>
+                        <label class="form-label" for="lastname">Sobrenome:</label>
+                        <input class="form-control" type="text" id="lastname" name="lastname" placeholder="Digite seu nome">
+                    </div>
+                    <div>
+                        <label class="form-label" for="password">Senha:</label>
+                        <input class="form-control" type="password" id="password" name="password" placeholder="Senha">
+                    </div>
+                    <div>
+                        <label class="form-label" for="password">Confirmação da senha:</label>
+                        <input class="form-control" type="password" id="password" name="password" placeholder="Confirme a senha">
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary">Entrar</button>
+                    </div>
+                </form>
             </div>
         </div>
 
