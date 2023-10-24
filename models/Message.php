c4d7bc2d769bdac3af08540cae01b53ff32bb915
YasminVargas <yasmiin.vargass@gmail.com>
2023-10-24 19:02:42 -0300

    updated Message

--- models/Message.php
+++ models/Message.php
@@ -8,8 +8,20 @@ public function __construct($url){
     $this->url = $url;
 }
 
-public function setMessage(){
+public function setMessage($msg, $type, $redirect = "index.php"){
     
+$_SERVER["msg"] = $msg;
+$_SERVER["type"] = $type;
+
+if($redirect != "back"){
+
+    Header("Location: $this->url" . $redirect);
+}else{
+    Header("Location: " . $_SERVER["HTTP_REFER"]);
+}
+
+
+
 }
 public function getMessage(){
 
